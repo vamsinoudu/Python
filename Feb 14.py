3dd2b9d1f93339dbf33130c03162e4eaf5af86ce
Vamsi <98730375+vamsinoudu@users.noreply.github.com>
2022-02-14 16:23:24 +0530

    Python basics practice

--- Feb 14.py
+++ Feb 14.py
@@ -27,8 +27,19 @@ sum2 = 7
 sum = sum1+sum2
 print(sum1,"+",sum2,"=",sum)
 
+differ = sum1-sum2
+print(sum1,"-",sum2,"=",differ)
+
+multi = sum1 * sum2
+print(sum1,"*",sum2,"=",multi)
+
+divide = sum1 / sum2
+print(sum1,"/",sum2,"=",divide)
 
 print("     2")
 print(" 1    ")
 print(' "SDK" stands for , "Software development kit."')
 print(' "POC" stands for "proof of concept."')
+Name = 'house number 34'
+print(Name.find("house"))
+print(Name.index("number"))
