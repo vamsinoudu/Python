2cf204444a1c14d00d9ead75104dc71a524b7763
Vamsi <98730375+vamsinoudu@users.noreply.github.com>
2022-02-14 16:11:48 +0530

    Add files via upload

---
+++ Feb 14.py
@@ -0,0 +1,34 @@
+print("hi")
+print("HEY")
+print('i am learning python')
+num1 = 12
+num2 = 10
+print(num1*num2)
+print(num1+num1)
+print("\t")
+print("1\t","2\t","3\t")
+"""
+bob writing python            # comment
+bob learning python
+
+
+"""
+#switching job
+
+print(""" HI how are you 
+can we      learn    the 
+       together
+ """)
+print("how are\t","doing now")
+
+
+sum1 = 14
+sum2 = 7
+sum = sum1+sum2
+print(sum1,"+",sum2,"=",sum)
+
+
+print("     2")
+print(" 1    ")
+print(' "SDK" stands for , "Software development kit."')
+print(' "POC" stands for "proof of concept."')
