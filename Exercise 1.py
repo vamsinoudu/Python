20d9bcfa8f2164f52e9cd1506e50e5db5a6bcbf8
Vamsi <98730375+vamsinoudu@users.noreply.github.com>
2022-02-15 15:15:10 +0530

    python Exercise

---
+++ Exercise 1.py
@@ -0,0 +1,67 @@
+
+#Exercise1
+name ="vamshi"
+student_number = "ST1234"
+email_address = "nouduvamsi@gmail.com"
+
+print(name)
+print(student_number)
+print(email_address)
+
+#Exercise2
+sum1 = 14
+sum2 = 7
+sum3= sum1+sum2
+print(sum1,"+", sum2, "=",sum3)
+
+differ = sum1-sum2
+print(sum1,"-",sum2,"=",differ)
+
+multi = sum1 * sum2
+print(sum1,"*",sum2,"=",multi)
+
+divide = sum1 / sum2
+print(sum1,"/",sum2,"=",divide)
+
+#Exercise3
+print("1\t","\n"," 2\t","\n","   3\t","\n","      4\t","\n","          5\t","\n")
+
+#Exercise4
+print('"SDK" stands for "Software Development Kit".')
+print('"IDE" stands for "Integrated Development Environment".')
+
+#Exercise5
+year = 1981
+print("You are born in ",year)
+age = 2020-year
+print("In the year 2020, you will be", age,"years old.")
+
+#Exercise 6
+Details = input("Please enter you name:")
+print("Hi", Details, "welcome to Python programming :")
+
+
+#Exercise 7
+money = float(input("enter the amount in pounds"))
+one_pound=1.35
+dollar=(money*one_pound)
+print("Convert pound into dollar",dollar)
+
+#Exercise 8
+name = input("Enter the name :")
+student_id = input("Enter your student_id :")
+Email = input("Enter you email address:")
+
+
+#Exercise 9
+
+year = int(input("Please enter your born year:"))
+print("your born in ",year)
+age = (2020-year)
+print("In the year 2020, you will be ", age ," years old.")
+
+
+
+
+
+
