e06c9272241765b77a60ca0cd98d5885d1f9856a
Vamsi <98730375+vamsinoudu@users.noreply.github.com>
2022-02-19 06:29:48 +0530

    Add files via upload
    Take values of length and breadth of a rectangle from user and check if it is square or not.

---
+++ feb17 ex 2.py
@@ -0,0 +1,8 @@
+# 2.Take values of length and breadth of a rectangle from user and check if it is square or not.
+
+length = int(input("Enter the value :"))
+Breadth = int(input("Enter the value :"))
+if length == Breadth:
+    print(" It`s square")
+else:
+    print("Not a square ")
