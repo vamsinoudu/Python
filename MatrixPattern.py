bc67c57ff4424535537de075a4884d1f14f85ea9
Vamsi <98730375+vamsinoudu@users.noreply.github.com>
2022-02-21 10:14:06 +0530

    Add files via upload
    python practice programme

---
+++ MatrixPattern.py
@@ -0,0 +1,19 @@
+"""
+Write a program that displays matrix of integer numbers.
+
+"""
+
+for i in range(1,10,3):
+    if i<10:
+        print(i,i+1,i+2)
+
+'''
+for i in range(1,10):
+    if(i%3==0):
+        print(i)
+    else:
+        print(i,end=" ")
+'''
+
+
+
