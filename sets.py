73de11619e658d4f147450d2d144141501620c6b
Vamsi <98730375+vamsinoudu@users.noreply.github.com>
2022-02-27 09:58:28 +0530

    Add files via upload
    sets

---
+++ sets.py
@@ -0,0 +1,12 @@
+#set
+#no order
+#no duplicate
+
+myset = set(["A","B"])
+print(myset)
+myset.update(["C","D","A"])
+print(myset)
+myset_1 = set([ ])
+print(myset_1)
+myset_1.update(["G","H"])
+print(myset_1)
