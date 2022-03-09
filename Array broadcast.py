cc948461ec0cb7e85211247c53a72bab72659cbc
Vamsi <98730375+vamsinoudu@users.noreply.github.com>
2022-03-09 12:25:47 +0530

    Array broadcasting

---
+++ Array broadcast.py
@@ -0,0 +1,14 @@
+import numpy as np
+#int to int:
+a = np.array([[0.0, 0.0, 0.0], [10.1, 10.2, 10.3]])
+b = np.array([[1.2, 1.3, 2.0]])
+print("First array and second array")
+print(a + b)
+
+#float and integer:
+c = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [10, 12, 13, 14]])
+d = np.array([[0.1, 1.2, 2.3, 3.4]])
+print("float and integer array")
+print(c + d)
+
+
