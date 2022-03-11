bf8e63b9d998c08568d0ea3087a8edab19f88970
Vamsi <98730375+vamsinoudu@users.noreply.github.com>
2022-03-11 10:35:58 +0530

    Figures and Axes

---
+++ Figures and Axes.py
@@ -0,0 +1,15 @@
+import matplotlib
+from matplotlib import pyplot as plt
+
+y = [1, 4, 9, 16, 25, 36, 49, 64]
+x1 = [1, 16, 30, 42,55, 68, 77, 88]
+x2 = [1, 6, 12, 18, 28, 40, 52, 65]
+fig = plt.figure()
+ax = fig.add_axes([0, 0, 1, 1])
+l1 = ax.plot(x1, y, 'ys-')  # solid Line with yellow colour and square marker
+l2 = ax.plot(x2, y, 'go--')  # dash Line with green colour and circle marker
+ax.legend(labels=('tv', 'Smartphone'), loc='lower right')  # legend placed at lower
+ax.set_title("Advertisement effect on sales")
+ax.set_xlabel('medium')
+ax.set_ylabel(' sales')
+plt.show()
